c259effceb099fc7f3d1f2c66524e80037cd706d
JD Grillo <jdgrillo@microsoft.com>
2023-08-09 16:06:07 -0700

    adding output binding

--- ServiceBusQueueTrigger1.cs
+++ ServiceBusQueueTrigger1.cs
@@ -3,13 +3,16 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
-namespace Company.Function
+namespace JD.Function
 {
     public class ServiceBusQueueTrigger1
     {
         [FunctionName("ServiceBusQueueTrigger1")]
-        public void Run([ServiceBusTrigger("jdsq", Connection = "jdsservicebus_SERVICEBUS")]string myQueueItem, ILogger log)
+        public void Run([ServiceBusTrigger("jdsq", Connection = "jdsservicebus_SERVICEBUS")]string myQueueItem, 
+        [Blob("outcontainer/sample-blob", Connection = "jdsdatalakesa_BLOB")] out string blobContent,
+        ILogger log)
         {
+            blobContent = myQueueItem;
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
         }
     }
