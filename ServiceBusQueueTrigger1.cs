b9f5b53900043e04322bdbee876258449065dacd
JD Grillo <jdgrillo@microsoft.com>
2023-08-10 14:45:15 -0700

    adding object to blob

--- ServiceBusQueueTrigger1.cs
+++ ServiceBusQueueTrigger1.cs
@@ -1,19 +1,32 @@
 using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
-namespace JD.Function
+namespace Azure.Messaging.ServiceBus
 {
     public class ServiceBusQueueTrigger1
     {
         [FunctionName("ServiceBusQueueTrigger1")]
-        public void Run([ServiceBusTrigger("jdsq", Connection = "jdsservicebus_SERVICEBUS")]string myQueueItem, 
-        [Blob("outcontainer/sample-blob", Connection = "jdsdatalakesa_BLOB")] out string blobContent,
-        ILogger log)
+        public void Run(
+            [ServiceBusTrigger("jdsq", Connection = "jdsservicebus_SERVICEBUS")]
+            string myQueueItem,
+            string MessageId,
+            long SequenceNumber,
+            [Blob("outcontainer/sample-blob", Connection = "jdsdatalakesa_BLOB")] out string blobContent,
+            ILogger log)
         {
-            blobContent = myQueueItem;
+            blobContent = new
+            {
+                MessageId,
+                SequenceNumber,
+                myQueueItem
+            }.ToString();
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {MessageId}");
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {SequenceNumber}");
         }
     }
 }
